13eb7a729a5eda8dc5fbf7c14a85471ed66345ea
xxmd <xxmd3720@gmail.com>
2026-02-02 18:12:33 +0800

    停止跟踪 Gradle 和 IDEA 配置文件

---
+++ Launcher/src/main/java/custom/launcher/OverlayWindowCore.java
@@ -0,0 +1,154 @@
+package custom.client;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.pm.ServiceInfo;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.google.android.libraries.launcherclient.ILauncherOverlay;
+import com.google.android.libraries.launcherclient.ILauncherOverlayCallback;
+
+import custom.common.util.LogUtil;
+
+public class OverlayWindowCore {
+    private Activity activity;
+    private View rootView;
+    private boolean serviceConnected = false;
+    private SafeLauncherOverlay safeLauncherOverlay;
+
+    public OverlayWindowCore(Activity activity, View rootView) {
+        this.activity = activity;
+        this.rootView = rootView;
+    }
+
+    public void bindService() {
+        String packageName = "com.google.android.googlequicksearchbox";
+        String serviceName = "com.google.android.apps.gsa.nowoverlayservice.DrawerOverlayService";
+        boolean serviceExist = isServiceExist(activity, packageName, serviceName);
+        LogUtil.debug("bindService serviceExist: " + serviceExist);
+        Intent intent = new Intent("com.android.launcher3.WINDOW_OVERLAY");
+        intent.setClassName(packageName, serviceName);
+        intent.setData(Uri.parse("app://somepath"));
+        boolean bindServiceSuccess = activity.bindService(intent, new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                ILauncherOverlay launcherOverlay = ILauncherOverlay.Stub.asInterface(service);
+                ILauncherOverlayCallback callback = new ILauncherOverlayCallback.Stub() {
+                    @Override
+                    public void overlayScrollChanged(float progress) throws RemoteException {
+                        LogUtil.debug("ILauncherOverlayCallback overlayScrollChanged progress: " + progress);
+                    }
+
+                    @Override
+                    public void overlayStatusChanged(int status) throws RemoteException {
+                        LogUtil.debug("ILauncherOverlayCallback overlayStatusChanged status: " + status);
+                    }
+
+                    @Override
+                    public void onServiceStatus(Bundle bundle) throws RemoteException {
+                        LogUtil.debug("ILauncherOverlaylauncherOverlay onServiceStatus bundle: " + bundle);
+                        int serviceStatus = bundle.getInt("service_status");
+                        serviceConnected = true;
+                        addScrollListener(rootView);
+                    }
+                };
+                Bundle bundle = buildAttachWindowBundle();
+                safeLauncherOverlay = new SafeLauncherOverlay(launcherOverlay);
+                safeLauncherOverlay.windowAttached2(bundle, callback);
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                LogUtil.debug("onServiceDisconnected name: " + name);
+                serviceConnected = false;
+            }
+        }, Context.BIND_AUTO_CREATE);
+        LogUtil.debug("bindServiceSuccess: " + bindServiceSuccess);
+    }
+
+    /**
+     * 判断指定 Service 是否存在（已注册在系统中）
+     *
+     * @param context     上下文
+     * @param packageName 服务所在的包名
+     * @param className   服务完整类名（包含包名）
+     * @return true = 存在，false = 不存在
+     */
+    public static boolean isServiceExist(Context context, String packageName, String className) {
+        try {
+            PackageManager pm = context.getPackageManager();
+
+            // 构造 ComponentName
+            ComponentName component = new ComponentName(packageName, className);
+
+            // 查询 ServiceInfo
+            ServiceInfo serviceInfo = pm.getServiceInfo(component, 0);
+
+            // 如果能拿到 ServiceInfo，说明服务存在
+            return serviceInfo != null;
+        } catch (PackageManager.NameNotFoundException e) {
+            // 包名或类名不存在 → 服务不存在
+            return false;
+        } catch (Exception e) {
+            // 其他异常（如权限问题），也视为不存在
+            LogUtil.error("ServiceCheck Check service failed", e);
+            return false;
+        }
+    }
+
+
+    private void addScrollListener(View rootView) {
+        HorizontalScrollMonitor scrollMonitor = new HorizontalScrollMonitor(activity);
+        int measuredWidth = rootView.getMeasuredWidth();
+        if (measuredWidth == 0) {
+            measuredWidth = rootView.getContext().getResources().getDisplayMetrics().widthPixels;
+        }
+        LogUtil.debug("measuredWidth: " + measuredWidth);
+        scrollMonitor.setMaxScrollDistance(measuredWidth);
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return scrollMonitor.onTouchEvent(event);
+            }
+        });
+        scrollMonitor.setOnHorizontalScrollListener(new HorizontalScrollMonitor.OnHorizontalScrollListener() {
+            @Override
+            public void onScrollStart() {
+                LogUtil.debug(String.format("onScrollStart"));
+                safeLauncherOverlay.startScroll();
+            }
+
+            @Override
+            public void onScrolling(float deltaX, float progress) {
+                LogUtil.debug(String.format("onScrolling deltaX: %f, progress: %f", deltaX, progress));
+                safeLauncherOverlay.onScroll(progress);
+            }
+
+            @Override
+            public void onScrollEnd(float deltaX, float velocityX) {
+                LogUtil.debug(String.format("onScrollEnd deltaX: %f, velocityX: %f", deltaX, velocityX));
+                safeLauncherOverlay.endScroll();
+            }
+        });
+    }
+
+    private Bundle buildAttachWindowBundle() {
+        Bundle bundle = new Bundle();
+        WindowManager.LayoutParams attributes = activity.getWindow().getAttributes();
+        bundle.putParcelable("layout_params", attributes);
+        Configuration configuration = activity.getResources().getConfiguration();
+        bundle.putParcelable("configuration", configuration);
+        return bundle;
+    }
+}
---
+++ Velvet/src/main/java/custom/velvet/window/OverlayWindowWrapper.java
@@ -0,0 +1,60 @@
+package custom.server.window;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.os.IBinder;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class OverlayWindowWrapper {
+    private Context context;
+    private IBinder token;
+    private Window innerWindow;
+
+    public OverlayWindowWrapper(Context context, IBinder token) {
+        this.context = context;
+        this.token = token;
+        init();
+    }
+
+    private void init() {
+        Dialog dialog = new Dialog(context, android.R.style.Theme_Light);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        innerWindow = dialog.getWindow();
+        if (innerWindow != null) {
+            innerWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            innerWindow.setStatusBarColor(Color.TRANSPARENT);
+            innerWindow.setNavigationBarColor(Color.TRANSPARENT);
+            innerWindow.setWindowManager(null, token, "Velvet-ViewPager", true);
+            WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+            layoutParams.type = WindowManager.LayoutParams.TYPE_DRAWN_APPLICATION;
+            layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+            layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
+            layoutParams.flags |= WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            layoutParams.dimAmount = 0.0f;
+            innerWindow.setAttributes(layoutParams);
+            if (Build.VERSION.SDK_INT >= 29) {
+                innerWindow.setNavigationBarContrastEnforced(false);
+                innerWindow.setStatusBarContrastEnforced(false);
+            }
+            if (Build.VERSION.SDK_INT >= 30) {
+                innerWindow.setDecorFitsSystemWindows(false);
+            }
+        }
+    }
+
+    public void setContentView(View view) {
+        innerWindow.setContentView(view, new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        innerWindow.getWindowManager().addView(innerWindow.getDecorView(), innerWindow.getAttributes());
+    }
+
+    public Window getWindow() {
+        return innerWindow;
+    }
+}
